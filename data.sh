e3e589009b28a19df4a16d61dbb016156be2b1ee
Ubuntu <ubuntu@ip-172-31-26-117.us-west-2.compute.internal>
2020-01-30 05:22:28 +0000

    add plot data

---
+++ data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+AVX=3x12
+for L1M in 30
+do
+for L1N in 36
+do
+for L1K in 32
+do
+for L2M in $(( 2 * L1M ))
+do
+for L2N in $(( 3 * L1N ))
+do
+for L2K in $(( 3 * L1K ))
+do
+L3M=840
+L3N=1512
+L3K=1344
+for i in 0 1 2 3 4 5 6 7 8 9
+do
+echo "nonsq-$AVX-$L1M-$L1N-$L1K-$L2M-$L2N-$L2K-$L3M-$L3N-$L3K.txt"
+make MY_OPT="-O4 -DSIMD_$AVX -DCACHELAYOUT -DL1_BLOCK_SIZE_M=$L1M -DL1_BLOCK_SIZE_N=$L1N -DL1_BLOCK_SIZE_K=$L1K -DL2_BLOCK_SIZE_M=$L2M -DL2_BLOCK_SIZE_N=$L2N -DL2_BLOCK_SIZE_K=$L2K -DL3_BLOCK_SIZE_M=$L3M -DL3_BLOCK_SIZE_N=$L3N -DL3_BLOCK_SIZE_K=$L3K" 1>/dev/null 2>&1
+./benchmark-blocked >data/data$i-$AVX-$L1M-$L1N-$L1K-$L2M-$L2N-$L2K-$L3M-$L3N-$L3K.txt
+done
+done
+done
+done
+done
+done
+done
--- data/visualize.py
+++ data/visualize.py
@@ -8,7 +8,7 @@ import numpy as np
 N = 10
 data = defaultdict(list)
 for i in range(N):
-    with open('data%d.txt' % (i + 1)) as input:
+    with open('data%d-3x12-30-36-32-60-108-96-840-1512-1344.txt' % i) as input:
         reader = csv.reader(input, delimiter='\t')
         for row in reader:
             data[int(row[0])].append(float(row[1]))
