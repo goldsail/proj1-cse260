59fa3c1d54d8911cbc3f94e51bf3d54ec78085a9
Ubuntu <ubuntu@ip-172-31-26-117.us-west-2.compute.internal>
2020-01-30 08:15:36 +0000

    add data.txt

--- data/visualize.py
+++ data/visualize.py
@@ -8,7 +8,7 @@ import numpy as np
 N = 10
 data = defaultdict(list)
 for i in range(N):
-    with open('data%d-3x12-30-36-32-60-108-96-840-1512-1344.txt' % i) as input:
+    with open('data%d-3x12_C-30-36-32-60-108-96-840-1512-1344.txt' % i) as input:
         reader = csv.reader(input, delimiter='\t')
         for row in reader:
             data[int(row[0])].append(float(row[1]))
@@ -18,4 +18,11 @@ labels, values = zip(*sorted(zip(labels, values)))
 plt.plot(labels, values)
 plt.xlabel('Square matrix size')
 plt.ylabel('Average throughput (GF/s)')
-plt.savefig('data.png')
+plt.savefig('data_C.png')
+
+with open('data.txt', 'w') as output:
+    writer = csv.writer(output, delimiter='\t')
+    for i in range(len(labels)):
+        size = labels[i]
+        speed = values[i]
+        writer.writerow([size, speed])
