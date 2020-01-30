2f8cbd1559a7d6149ea70cbd24555b7670b6867a
Fan JIN <kingium@outlook.com>
2020-01-30 08:10:39 +0000

    change mean to max out of 10 tests

--- data/visualize.py
+++ data/visualize.py
@@ -8,14 +8,14 @@ import numpy as np
 N = 10
 data = defaultdict(list)
 for i in range(N):
-    with open('data%d-3x12_D-30-36-32-60-108-96-840-1512-1344.txt' % i) as input:
+    with open('data%d-3x12-30-36-32-60-108-96-840-1512-1344.txt' % i) as input:
         reader = csv.reader(input, delimiter='\t')
         for row in reader:
             data[int(row[0])].append(float(row[1]))
 
-labels, values = data.keys(), [np.mean(x) for x in data.values()]
+labels, values = data.keys(), [np.max(x) for x in data.values()]
 labels, values = zip(*sorted(zip(labels, values)))
 plt.plot(labels, values)
 plt.xlabel('Square matrix size')
 plt.ylabel('Average throughput (GF/s)')
-plt.savefig('data_D.png')
+plt.savefig('data.png')
